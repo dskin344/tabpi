0762d82d78d97247422d3cd2d7a9b3dfcbd2958a
Matthew Hyatt <mhyatt000@gmail.com>
2026-02-05 11:44:43 -0600

    move cartpole script

--- scripts/cartpole.py
+++ scripts/cartpole.py
@@ -1,7 +1,98 @@
 from __future__ import annotations
 
+import gymnasium as gym
+import imageio
+from imitation_datasets.dataset import BaselineDataset
+import numpy as np
+from rich import print
+from sklearn.metrics import mean_squared_error, r2_score
+from tabpfn import TabPFNRegressor
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
 
 def main():
-    pass
-    # ds = BaselineDataset("NathanGavenski/CartPole-v1", source="huggingface", n_episodes=100)
-    # i: wq
+    ds = BaselineDataset("NathanGavenski/CartPole-v1", source="huggingface", n_episodes=100)
+    dload = DataLoader(ds, batch_size=32, shuffle=True)
+
+    ds_eval = BaselineDataset("NathanGavenski/CartPole-v1", source="huggingface", n_episodes=100, split="eval")
+
+    print(ds)
+    print(len(ds))
+    print(ds.average_reward, ds.states.shape)
+
+    # state, action, next_state = ds[0]
+    # print(state.shape, action.shape, next_state.shape)
+    print(ds.states.shape, ds.actions.shape)
+    print(type(ds.states))
+
+    print(ds.actions)
+
+    regressor = TabPFNRegressor()  # Uses TabPFN-2.5 weights, trained on synthetic data only.
+    n_train, n_eval = 5000, 1000
+    state_t, action_t = ds.states[0:n_train], ds.actions[0:n_train]
+    regressor.fit(state_t, action_t)
+    print("done fitting")
+    # quit()
+
+    # Predict on the test set
+    state_e, action_e = (
+        ds_eval.states[0:n_eval],
+        ds_eval.actions[0:n_eval],
+    )
+    print("eval shapes", state_e.shape, action_e.shape)
+    yh = regressor.predict(state_e)
+
+    # Evaluate the model
+    mse = mean_squared_error(action_e, yh)
+    r2 = r2_score(action_e, yh)
+
+    print("Mean Squared Error (MSE):", mse)
+    print("R² Score:", r2)
+
+    #
+    #
+    #
+
+    policy = lambda obs: regressor.predict(obs.reshape(1, -1))[0]
+    env = gym.make("CartPole-v1", render_mode="rgb_array")  # drop render_mode if headless
+    obs, _info = env.reset(seed=0)
+
+    done = False
+    totals = []
+    rewards = []
+    frames = []
+
+    for i in tqdm(range(10)):
+        # while not done:
+        for _ in tqdm(range(500)):
+            frame = env.render()
+            frames.append(frame)
+
+            # --- call your policy ---
+            action = policy(obs)  # must be 0 or 1
+            print("predicted action:", action)
+            # action = int(action)
+            action = (action >= 0.5).astype(int)
+            print("converted action:", action)
+
+            obs, reward, terminated, truncated, _info = env.step(action)
+            done = terminated or truncated
+            rewards.append(reward)
+            print(f"terminated: {terminated}, truncated: {truncated}, done: {done}, reward: {reward}")
+            if done:
+                break
+
+        print(f"resetting env after episode {i} with total reward {sum(rewards)}")
+        totals.append(sum(rewards))
+        rewards = []
+        obs, _info = env.reset()
+
+    env.close()
+    print("Average total reward over 10 episodes:", np.mean(totals))
+    imageio.mimsave("cartpole.mp4", frames, fps=50)
+    print("Episode(s) saved to video cartpole.mp4")
+
+
+if __name__ == "__main__":
+    main()
